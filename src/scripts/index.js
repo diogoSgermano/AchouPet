2999aee16a5c52915c484337a1bb29794b378edb
Leticia Suelen Carvalho Germano <diogogermano28@gmail.com>
2025-09-29 19:34:56 -0300

    add handlebars
    Co-Authored-By: Leticia <180476917+leticiascg@users.noreply.github.com>

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,7 @@ import exphbs from "express-handlebars";
 const app = express();
 
 // Configuração do Handlebars
-app.engine("handlebars", exphbs.engine({defaultLayout: "home"}));
+app.engine("handlebars", exphbs.engine({defaultLayout: "index"}));
 app.set("view engine", "handlebars");
 
 app.use(express.static("src/public"));
@@ -13,10 +13,21 @@ app.get("/", function(_req, res) {
   res.render("inicio");
 });
 
+
 app.get("/animais", function(_req, res) {
   res.render("animais");
-} );
+});
+
+app.get("/login", function(_req, res) {
+  // Aqui, especificamos que a view 'login' deve usar o layout 'main'
+  res.render("login", { layout: "login" });
+});
+
+app.get("/comentarios", function(_req, res) {
+  // Como não especificamos um layout, ele usará o padrão: 'nav-foot'
+  res.render("comentarios");
+});
+
 app.listen(8080, () => {
   console.log("Servidor rodando em http://localhost:8080");
 });
-
