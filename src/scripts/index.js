63045d28b1e40d5f692278f29e76c45a6e53e357
Leticia Suelen Carvalho Germano <diogogermano28@gmail.com>
2025-10-07 13:24:38 -0300

    edit pagina inicial

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,22 @@
 import express from "express";
 import exphbs from "express-handlebars";
+import mysql from "mysql2";
 
 const app = express();
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "diogoGer26",
+  password: "26Diogo1024.",
+  database: "achoupet",
+});
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Conectado ao banco de dados MySQL!");
+});
+
 // Configuração do Handlebars
-app.engine("handlebars", exphbs.engine({defaultLayout: "index"}));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "index" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static("src/public"));
@@ -13,12 +25,9 @@ app.get("/", function(_req, res) {
   res.render("inicio");
 });
 
-
 app.get("/animais", function(_req, res) {
   res.render("animais");
 });
-
-
 
 app.get("/signIn", function(_req, res) {
   // Aqui, especificamos que a view 'login' deve usar o layout 'main'
